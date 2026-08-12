3c44255b143f910d3e2fe48918c53ca1adcdcaa6
morroman123 <anthonymorgan6674@gmail.com>
2026-08-11 17:45:35 -0700

    Update aicombat.cpp

--- apps/openmw/mwmechanics/aicombat.cpp
+++ apps/openmw/mwmechanics/aicombat.cpp
@@ -616,8 +616,10 @@ namespace MWMechanics
                 /////////////////////////////////////////////////////////////////
                 //storage.stopAttack();//base
                  //pathTo(actor, PathFinder::makeOsgVec3(storage.mFleeDest), duration)
+                forceFlee = true;
                     storage.startFleeing();
                 MWBase::Environment::get().getDialogueManager()->say(actor, "flee");
+                
                 //storage.mMovement.mPosition[0] = Misc::Rng::rollProbability() < 0.5 ? -0.35f : 0.35f;//active
                 //storage.mMovement.mPosition[1] = -1.f;//active
                 //characterController.setAttackingOrSpell(false);//base
