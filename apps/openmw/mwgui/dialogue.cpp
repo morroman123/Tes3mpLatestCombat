94b21b89bfa5dac66be7ce288c1769d3fa1bc0d0
morroman123 <anthonymorgan6674@gmail.com>
2026-08-18 15:31:32 -0700

    Update dialogue.cpp

--- apps/openmw/mwgui/dialogue.cpp
+++ apps/openmw/mwgui/dialogue.cpp
@@ -824,9 +824,9 @@ namespace MWGui
         //MWWorld::Ptr player = MWMechanics::getPlayer();
         MWWorld::Ptr playerPtr = MWMechanics::getPlayer();//EDIT
         float personality = playerPtr.getClass().getCreatureStats(playerPtr).getAttribute(ESM::Attribute::Personality).getModified();//edit
-        //float persMult = (55.f + (personality / 2.2f));
-        float persMult = std::min(100,(55.f + (personality / 2.2f)));
-        int persMultInt = static_cast<int>(persMult);
+        float persMult = (55.f + (personality / 2.2f));
+        //float persMult = std::min(100,(55.f + (personality / 2.2f)));
+        int persMultInt = std::min(100,(static_cast<int>(persMult)));
         //std::string DispoMax = std::format("/{}",persMult);
         //std::ostringstream ss = << "Age: " << persMult << ";
     //ss << "Age: " << persMult << ";
