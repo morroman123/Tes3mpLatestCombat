f38e4a370bdec8d4cef786d20ceb9abe052e95cc
morroman123 <anthonymorgan6674@gmail.com>
2026-08-18 15:04:26 -0700

    Update mechanicsmanagerimp.cpp

--- apps/openmw/mwmechanics/mechanicsmanagerimp.cpp
+++ apps/openmw/mwmechanics/mechanicsmanagerimp.cpp
@@ -582,8 +582,9 @@ namespace MWMechanics
         float personality = playerPtr.getClass().getCreatureStats(playerPtr).getAttribute(ESM::Attribute::Personality).getModified();//edit
         float persMult = (55.f + (personality / 2.2f));
         //int intTest = 100;
-
-        int effective_disposition = std::max(0,std::min(int(x),int(persMult)));//, normally clamped to [0..100] when used//edit
+        //std::min(100,int(persMult))
+        //int effective_disposition = std::max(0,std::min(int(x),int(persMult)));//, normally clamped to [0..100] when used//edit
+        int effective_disposition = std::max(0,std::min(int(x),std::min(100,int(persMult))));//, normally clamped to [0..100] when used//edit
         return effective_disposition;
     }
 
